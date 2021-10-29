54dd3e146fdaeaa7b5735d57deb8c4a02d806ff8
Timothy Luebke <luebketj@gmail.com>
2021-10-29 12:04:02 -0600

    added another image and functionality with it

--- profile.js
+++ profile.js
@@ -1,10 +1,13 @@
 let colorBtn = document.querySelector("#color");
 let placeBtn = document.querySelector('#place');
 let ritualBtn = document.querySelector('#ritual');
-
+let classyDuck = document.querySelector('.classy-duck');
+console.log(classyDuck);
 colorBtn.addEventListener('click', favoriteColor);
 placeBtn.addEventListener('click', favoritePlace);
 ritualBtn.addEventListener('click', favoriteRitual);
+
+window.onscroll = function() {scrollCalls()};
 
 
 function favoriteColor(){
@@ -16,3 +19,17 @@ function favoritePlace(){
 function favoriteRitual(){
     alert("My Favorite Ritual is going to church on Sundays")
 }
+function scrollCalls(){
+    if(document.documentElement.scrollTop > 200){
+        console.log(classyDuck.classList.contains('hidden-duck'));
+        if(classyDuck.classList.contains('hidden-duck')){
+            classyDuck.classList.remove('hidden-duck');
+        }
+    }
+    else{
+        if(!classyDuck.classList.contains('hidden-duck')){
+            classyDuck.classList.add('hidden-duck');
+        }
+    }
+}
+
